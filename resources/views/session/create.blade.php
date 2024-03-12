d926ecf3161a2615d79d5479b4fdf4fb821c6cdc
bruno <brunovicente.dev@gmail.com>
2024-03-11 21:38:07 -0300

    Added Entrar screen

--- resources/views/session/create.blade.php
+++ resources/views/session/create.blade.php
@@ -1,16 +1,40 @@
 <x-layout>
 
-    <main class="max-w-5xl mx-auto bg-red-500 flex justify-center items-center h-screen">
-        <section class="w-full h-4/5 bg-blue-300 flex relative rounded-3xl">
-            <div class="w-1/2 flex flex-col justify-center items-center bg-green-300 rounded-l-3xl">
-                <p>Hello World</p>
-                <button class="text-blue-700 signinBtn">switch</button>
+    <main class="max-w-5xl mx-auto flex justify-center items-center h-screen">
+        <section class="w-full h-4/5 drop-shadow-lg flex relative rounded-3xl container-sign">
+            
+            <div class="w-1/2 flex flex-col justify-center items-center rounded-l-3xl">
+                
+                <a class="text-blue-700 signinBtn cursor-pointer">switch</a>
             </div>
-            <div class="w-1/2 flex flex-col justify-center items-center bg-purple-300 rounded-r-3xl">
-                <p>Hello World</p>
-                <button class="text-blue-700 signupBtn">switch</button>
+
+            <div class="w-1/2 flex flex-col justify-center px-20 rounded-r-3xl">
+                <h1 class="text-5xl hanalei text-vermelho mb-5 drop-shadow-md">Entrar</h1>
+                <form action="" method="POST">
+                    @csrf
+                    <div class="flex flex-col mb-3">
+                        <label for="email" class="poppins text-laranja-escuro drop-shadow-md font-semibold">E-mail:</label>
+                        <input type="text" name="email" placeholder="E-mail" class="p-2 rounded-lg drop-shadow-md">
+                    </div>
+                    <div class="flex flex-col mb-3">
+                        <label for="senha" class="poppins text-laranja-escuro drop-shadow-md font-semibold">Senha:</label>
+                        <input type="text" name="senha" placeholder="Senha" class="p-2 rounded-lg drop-shadow-md">
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <p class="poppins text-xs">
+                            Não possui uma conta?
+                            <a class="text-azul signupBtn cursor-pointer">Cadastre-se</a>
+                        </p>
+                        <button type="submit" 
+                            class="poppins text-white rounded-lg px-5 py-2 uppercase font-bold drop-shadow-md bg-btn-sign">
+                            <span class="drop-shadow-md">Entrar</span>
+                        </button>
+                    </div>
+                </form>
             </div>
-            <div class="banner-login absolute w-1/2 h-full z-50 rounded-l-3xl">
+
+            {{-- Slider's Image --}}
+            <div class="banner-sign absolute w-1/2 h-full z-50 rounded-l-3xl">
 
             </div>
         </section>
