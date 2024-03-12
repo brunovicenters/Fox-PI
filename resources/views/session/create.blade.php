bfb69b4d876490202ef8ee7fa025e3cba3cd9ebf
bruno <brunovicente.dev@gmail.com>
2024-03-12 11:47:10 -0300

    Added forms

--- resources/views/session/create.blade.php
+++ resources/views/session/create.blade.php
@@ -3,18 +3,36 @@
     <main class="max-w-5xl mx-auto flex justify-center items-center h-screen">
         <section class="w-full h-4/5 drop-shadow-lg flex relative rounded-3xl container-sign">
             
-            <div class="w-1/2 flex flex-col justify-center items-center rounded-l-3xl">
-                
-                <a class="text-blue-700 signinBtn cursor-pointer">switch</a>
+            {{-- Cadastrar --}}
+            <div class="w-1/2 flex flex-col justify-center px-20 rounded-l-3xl">
+                <h1 class="text-5xl hanalei text-vermelho mb-5 drop-shadow-md">Cadastrar</h1>
+                <form action="/cadastrar" method="POST">
+                    @csrf
+                    <x-form.input-group label="Nome" name="nome" placeholder="Nome" title="Escreva seu nome" />
+                    <x-form.input-group label="CPF" name="cpf" type="number" placeholder="00000000000" title="Escreva somente números"/>
+                    <x-form.input-group label="E-mail" name="email" type="email" placeholder="E-mail" />
+                    <x-form.input-group label="Senha" name="senha" type="password" placeholder="Senha" title="Escreva uma senha segura e anote-a" />
+                    <div class="flex items-center justify-between">
+                        <p class="poppins text-xs">
+                            Já possui uma conta?
+                            <span class="text-azul signinBtn cursor-pointer">Entre</span>
+                        </p>
+                        <button type="submit"
+                            class="poppins text-white rounded-lg px-5 py-2 uppercase font-bold drop-shadow-md bg-btn-sign">
+                            <span class="drop-shadow-md">Cadastrar</span>
+                        </button>
+                    </div>
+                </form>
             </div>
 
+            {{-- Entrar --}}
             <div class="w-1/2 flex flex-col justify-center px-20 rounded-r-3xl">
                 <h1 class="text-5xl hanalei text-vermelho mb-5 drop-shadow-md">Entrar</h1>
-                <form action="" method="POST">
+                <form action="/entrar" method="POST">
                     @csrf
                     <div class="flex flex-col mb-3">
                         <label for="email" class="poppins text-laranja-escuro drop-shadow-md font-semibold">E-mail:</label>
-                        <input type="text" name="email" placeholder="E-mail" class="p-2 rounded-lg drop-shadow-md">
+                        <input type="text" name="email" placeholder="E-mail" class="p-2 rounded-lg drop-shadow-md text-laranja-escuro">
                     </div>
                     <div class="flex flex-col mb-3">
                         <label for="senha" class="poppins text-laranja-escuro drop-shadow-md font-semibold">Senha:</label>
@@ -23,9 +41,9 @@
                     <div class="flex items-center justify-between">
                         <p class="poppins text-xs">
                             Não possui uma conta?
-                            <a class="text-azul signupBtn cursor-pointer">Cadastre-se</a>
+                            <span class="text-azul signupBtn cursor-pointer">Cadastre-se</span>
                         </p>
-                        <button type="submit" 
+                        <button type="submit"
                             class="poppins text-white rounded-lg px-5 py-2 uppercase font-bold drop-shadow-md bg-btn-sign">
                             <span class="drop-shadow-md">Entrar</span>
                         </button>
