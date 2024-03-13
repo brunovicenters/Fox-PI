bd3179ac0404cf976655fea6d5a73aff9fa8dbec
bruno <brunovicente.dev@gmail.com>
2024-03-12 21:43:32 -0300

    Added navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,39 @@
+<main class="navbar w-full h-28 relative flex justify-between">
+    <img class="absolute right-0 top-0" src="images/nav-desenho1.png" alt="Enfeite da Navbar">
+    <img class="absolute -left-1 bottom-0" src="images/nav-desenho2.png" alt="Enfeite da Navbar">
+
+    <div>
+        <img class="ml-12 w-48" src="images/logo.png" alt="Logo do site">
+    </div>
+
+    <div>
+        <nav class="flex gap-3 text-white">
+            <a href="/" class="hanalei font-semibold uppercase menu-nav">
+                Início
+            </a>
+            |
+            <a href="/" class="hanalei font-semibold uppercase menu-nav">
+                Rua Lugar Algum, 781 - 04562-708
+            </a>
+            |
+            <a href="/" class="hanalei font-semibold uppercase menu-nav">Fale conosco</a>
+            |
+            <div class="flex gap-2 justify-center items-center">
+                <a href="/" class="hanalei font-semibold uppercase menu-nav">
+                    <img class="w-5 icon" src="images/facebook-icon.svg" alt="Facebook icon">
+                </a>
+                <a href="/" class="hanalei font-semibold uppercase menu-nav">
+                    <img class="w-5" src="images/instagram-icon.svg" alt="Instagram icon">
+                </a>
+                <a href="/" class="hanalei font-semibold uppercase menu-nav">
+                    <img class="w-5" src="images/whats-icon.svg" alt="Whatsapp icon">
+                </a>
+            </div>
+        </nav>
+
+    </div>
+
+    <div>
+
+    </div>
+</main>
