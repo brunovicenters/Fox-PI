ae212302e35b30b5c72223dfd54d4ba5504fda14
bruno <brunovicente.dev@gmail.com>
2024-03-14 22:06:19 -0300

    Added search bar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,7 +7,7 @@
     </div>
 
     <div class="w-2/3 mt-3">
-        <nav class="flex gap-3 text-white">
+        <nav class="flex items-center gap-3 text-white">
             <a href="/" class="hanalei uppercase menu-nav">
                 Início
             </a>
@@ -37,16 +37,27 @@
             </div>
         </nav>
 
-        <form action="#" method="get" class="mt-3">
-            <input
+        <form action="#" method="get" class="mt-3 relative">
+            <div class="w-3/4 relative">
+
+                <input
                 type="text"
                 name="search"
                 id="search"
-                class="w-3/4 h-10 rounded-full">
+                class="w-full h-10 rounded-full input-form pl-3 pr-10 py-2 drop-shadow-md text-laranja-escuro">
+                <button type="submit" class="absolute right-3 top-1 rotate-90 rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="#D36411" class="w-8 h-8 rounded-full p-1 search">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                    </svg>
+                </button>
+            </div>
         </form>
     </div>
 
     <div>
-
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+          </svg>
+          
     </div>
 </main>
