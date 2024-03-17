13c25f1fd46de56f77ee6fee84fa832ed7c53cf3
bruno <brunovicente.dev@gmail.com>
2024-03-17 14:30:24 -0300

    Added menu and hover animations to icons

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -58,15 +58,30 @@
 
     <div class="flex gap-10 justify-center items-center pr-20">
         <a class="relative pointer" href="/carrinho">
-            <span class="absolute -top-3 -left-2 text-xl rounded-full text-center w-8 h-8 p-1 z-50 text-amarelo shopcart-icon-number">0</span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-16 h-16 -scale-x-100">
+            <span class="absolute -top-3 -left-2 text-xl rounded-full flex justify-center items-center w-8 h-8 p-1 z-50 text-amarelo shopcart-icon-number">0</span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-16 h-16 -scale-x-100 icon-stroke">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
             </svg>
         </a>
-        <div class="pointer">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-20 h-20">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            </svg>
+        <div class="relative">
+            <div class="relative">
+                <button id="menu-btn" onclick="toggleMenu()" class="absolute w-full h-full">
+                </button>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-20 h-20 icon-stroke">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+            </div>
+
+            <div id="menu" class="absolute z-50 top-50 -right-10 rounded-lg hidden menu">
+                <ul class="hanalei text-xl text-white text-center w-36 flex flex-col space-y-1 px-3 py-1">
+                    <li ><a class="menu-item" href="/meus-pedidos">Meus pedidos</a></li>
+                    <li class="w-full h-1 bg-white"></li>
+                    <li><a class="menu-item" href="/meu-perfil">Minha conta</a></li>
+                    <li class="w-full h-1 bg-white"></li>
+                    <li><a href="/sair" class="menu-item">Sair</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </main>
+<script src="scripts/layout/navbar.js"></script>
