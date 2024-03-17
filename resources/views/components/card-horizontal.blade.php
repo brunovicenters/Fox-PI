3651e5470880438dbad2dca7273ebc066d7b0761
bruno <brunovicente.dev@gmail.com>
2024-03-17 15:37:02 -0300

    Added meus pedidos

---
+++ resources/views/components/card-horizontal.blade.php
@@ -0,0 +1,3 @@
+<div class="card-horizontal flex justify-between items-center py-2 px-5 rounded-lg w-full">
+    {{ $slot }}
+</div>
--- resources/views/pedidos/index.blade.php
+++ resources/views/pedidos/index.blade.php
@@ -2,7 +2,14 @@
 
     <x-navbar />
     
-    <main>
-        <h1 class="hanalei text-6xl drop-shadow-md uppercase">Meus pedidos</h1>
+    <main class="max-w-5xl mx-auto text-roxo mt-10 mb-3">
+        <h1 class="hanalei text-6xl drop-shadow-md uppercase mb-5">Meus pedidos</h1>
+        <div class="flex flex-col space-y-3">
+            @for ($i = 1; $i <= 3; $i++)
+                <a href="#" class="pointer hover:scale-105 ease-in-out duration-300 w-3/4">
+                    <x-pedidos.card-pedidos status="{{ $i }}" />
+                </a>
+            @endfor
+        </div>
     </main>
 </x-layout>
