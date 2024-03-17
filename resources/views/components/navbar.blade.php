3f793a3fc6b6b1ef5442af0a91ce82c7edee1c10
bruno <brunovicente.dev@gmail.com>
2024-03-17 10:02:57 -0300

    Fixed navbar structure

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<main class="navbar w-full h-28 relative flex justify-between">
+<main class="navbar w-full h-28 relative flex gap-16">
     <img class="absolute right-0 top-0" src="images/nav-desenho1.png" alt="Enfeite da Navbar">
     <img class="absolute -left-1 bottom-0" src="images/nav-desenho2.png" alt="Enfeite da Navbar">
 
@@ -6,7 +6,7 @@
         <img class="ml-12 w-48" src="images/logo.png" alt="Logo do site">
     </div>
 
-    <div class="w-2/3 mt-3">
+    <div class="w-2/4 mt-3">
         <nav class="flex items-center gap-3 text-white">
             <a href="/" class="hanalei uppercase menu-nav">
                 Início
@@ -54,12 +54,17 @@
         </form>
     </div>
 
-    <div class="flex gap-2 justify-center items-center pr-5">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-18 h-18">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-        </svg>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-20 h-20">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-        </svg>
+    <div class="flex gap-10 justify-center items-center pr-20">
+        <a class="relative pointer" href="/carrinho">
+            <span class="absolute -top-3 -left-2 text-xl rounded-full text-center w-8 h-8 p-1 z-50 text-amarelo shopcart-icon-number">0</span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-16 h-16 -scale-x-100">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+            </svg>
+        </a>
+        <div class="pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-20 h-20">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+        </div>
     </div>
 </main>
