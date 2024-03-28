88d85b2be59afc0acf7606addde5fd82772f2dff
bruno <brunovicente.dev@gmail.com>
2024-03-28 15:24:29 -0300

    Added first row footer

--- resources/views/components/footer/footer.blade.php
+++ resources/views/components/footer/footer.blade.php
@@ -1,31 +1,70 @@
 <div class="divisoria-footer h-36"></div>
 <footer class="footer w-full p-10">
-   <div>
-        <div class="footer-logo">
-            <div>
-                <a href="/">
-                    <img class="ml-12 w-48" src="images/logo.png" alt="Logo do site">
-                </a>
+   <div class="max-w-5xl mx-auto">
+       <div class="flex justify-between">
+            <div class="footer-logo">
+                <div>
+                    <a href="/">
+                        <img class="ml-12 w-48" src="images/logo.png" alt="Logo do site">
+                    </a>
+                </div>
             </div>
 
-        </div>
+            <div class="footer-conta">
+                {{-- Carrinho / Minha Conta / Meus Pedidos --}}
+                <h3 class="text-laranja-claro text-2xl hanalei">Minha conta</h3>
+                <ul>
+                    <li class="poppins text-verde">Minha conta</li>
+                    <li class="poppins text-verde">Meu carrinho</li>
+                    <li class="poppins text-verde">Meus pedidos</li>
+                </ul>
+            </div>
 
-        <div class="footer-conta">
-            {{-- Carrinho / Minha Conta / Meus Pedidos --}}
-        </div>
-
-        <div class="footer-card">
-            {{-- Loja --}}
-            {{-- SAC --}}
-        </div>
-   </div>
-   <div>
-        <div class="footer-payment">
-            {{-- Formas de pagamento --}}
-        </div>
-        <div class="footer-social">
-            {{-- Início --}}
-            {{-- Redes --}}
-        </div>
+            <div id="footer-cards" class="footer-cards flex flex-col space-y-5">
+                {{-- Loja --}}
+                <div>
+                    <h3 class="text-laranja-claro text-2xl hanalei">Loja</h3>
+                    <div class="footer-card rounded-lg flex space-x-2 py-2 px-3">
+                        <div class="footer-card-icon flex items-center justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#B20D30" class="w-10 h-10">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
+                            </svg>
+                        </div>
+                        <div class="footer-card-text flex flex-col">
+                            <p class="text-vermelho hanalei">Rua Lugar Algum, 761, SP</p>
+                            <p class="text-verde poppins text-xs">04562-708</p>
+                            <p class="text-verde poppins uppercase text-xs">Seg a Sex - 08h a 18h</p>
+                            <p class="text-verde poppins text-xs uppercase">&#40;Exceto feriado e emenda de feriado&#41;</p>
+                        </div>
+                    </div>
+                </div>
+                {{-- SAC --}}
+                <div>
+                    <h3 class="text-laranja-claro text-2xl hanalei">Central de Atendimento</h3>
+                    <div class="footer-card rounded-lg flex space-x-2 py-2 px-3">
+                        <div class="footer-card-icon flex items-center justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#B20D30" class="w-10 h-10">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                            </svg>
+                        </div>
+                        <div class="footer-card-text flex flex-col">
+                            <a href="/fale-conosco" class="hanalei text-vermelho hover:underline">Fale Conosco</a>
+                            <p class="text-verde poppins text-xs">+55 11 5839-7006</p>
+                            <p class="text-verde poppins uppercase text-xs">Seg a Sex - 08h a 18h</p>
+                            <p class="text-verde poppins text-xs uppercase">&#40;Exceto feriado e emenda de feriado&#41;</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       </div>
+       <div>
+            <div class="footer-payment">
+                {{-- Formas de pagamento --}}
+            </div>
+            <div class="footer-social">
+                {{-- Início --}}
+                {{-- Redes --}}
+            </div>
+       </div>
    </div>
 </footer>
