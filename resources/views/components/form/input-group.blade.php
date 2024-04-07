50914c05453b5482ab06967ef87ef688f02e8e8a
bruno <brunovicente.dev@gmail.com>
2024-04-07 01:08:14 -0300

    Changed id attribute

--- resources/views/components/form/input-group.blade.php
+++ resources/views/components/form/input-group.blade.php
@@ -1,11 +1,11 @@
-@props(['label' => '', 'placeholder' => '', 'name' => '', 'type' => 'text'])
+@props(['label' => '', 'placeholder' => '', 'name' => '', 'type' => 'text', 'id' => $name])
 
 <div class="flex flex-col mb-3">
     <label for="{{ $name }}" class="poppins text-laranja-escuro drop-shadow-md font-semibold">{{ $label }}:</label>
     <input
         type="{{ $type }}"
         name="{{ $name }}"
-        id="{{ $name }}"
+        id="{{ $id }}"
         placeholder="{{ $placeholder }}"
         class="p-2 rounded-lg drop-shadow-md text-laranja-escuro input-form"
         required
