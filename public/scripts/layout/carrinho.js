1437646332c9cac9b092fc8309be039393a79fa2
bruno <brunovicente.dev@gmail.com>
2024-04-06 22:57:17 -0300

    Added mascaraCEP

---
+++ public/scripts/layout/carrinho.js
@@ -0,0 +1,11 @@
+function mascaraCEP(i) {
+    let v = i.value;
+
+    if (isNaN(v[v.length - 1])) {
+        i.value = v.substring(0, v.length - 1);
+        return;
+    }
+
+    i.setAttribute("maxlength", "9");
+    if (v.length == 5) i.value += "-";
+}
