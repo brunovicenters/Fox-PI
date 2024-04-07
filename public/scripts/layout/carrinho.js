80f3e46971b9c6905126016775a5f40fb1a96170
bruno <brunovicente.dev@gmail.com>
2024-04-07 01:36:55 -0300

    Added viacep api

--- public/scripts/layout/carrinho.js
+++ public/scripts/layout/carrinho.js
@@ -9,3 +9,58 @@ function mascaraCEP(i) {
     i.setAttribute("maxlength", "9");
     if (v.length == 5) i.value += "-";
 }
+
+function limparCampos() {
+    document.querySelector("#cep").value = "";
+    document.querySelector("#rua").value = "";
+    document.querySelector("#numero").value = "";
+    document.querySelector("#bairro").value = "";
+    document.querySelector("#cidade").value = "";
+    document.querySelector("#estado").value = "";
+    document.querySelector("#nome_end").value = "";
+    document.querySelector("#complemento").value = "";
+}
+
+const limpar = document.querySelector("#limpar");
+
+limpar.addEventListener("click", limparCampos);
+
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        document.getElementById("rua").value = conteudo.logradouro;
+        document.getElementById("bairro").value = conteudo.bairro;
+        document.getElementById("cidade").value = conteudo.localidade;
+        document.getElementById("estado").value = conteudo.uf;
+    } else {
+        limparCampos();
+        alert("CEP não encontrado.");
+    }
+}
+
+function pesquisacep(valor) {
+    let cep = valor.replace(/\D/g, "");
+
+    if (cep != "") {
+        let validacep = /^[0-9]{8}$/;
+        if (validacep.test(cep)) {
+            document.getElementById("rua").value = "...";
+            document.getElementById("bairro").value = "...";
+            document.getElementById("cidade").value = "...";
+            document.getElementById("estado").value = "...";
+
+            let script = document.createElement("script");
+
+            script.src =
+                "https://viacep.com.br/ws/" +
+                cep +
+                "/json/?callback=meu_callback";
+
+            document.body.appendChild(script);
+        } else {
+            //cep é inválido.
+            limparCampos();
+            alert("CEP inválido.");
+        }
+    }
+}
--- resources/views/carrinho/endereco.blade.php
+++ resources/views/carrinho/endereco.blade.php
@@ -9,43 +9,49 @@
                 @csrf
                 <div class="flex justify-between">
                     <div class="w-3/12">
-                        <x-form.input-group label="CEP" name="cep"
+                        <x-form.input-group label="CEP" name="cep" id="cep"
                             type="text"
                             maxlength="9"
                             oninput="mascaraCEP(this)"
                             placeholder="00000-000"
-                            title="Escreva somente números"/>
+                            title="Escreva somente números"
+                            onblur="pesquisacep(this.value);"/>
                     </div>
                     <div class="w-6/12">
-                        <x-form.input-group label="Rua" name="ENDERECO_LOGRADOURO" placeholder="Rua Lugar Algum" title="Escreva o nome da sua rua"/>
+                        <x-form.input-group id="rua" label="Rua" name="ENDERECO_LOGRADOURO" placeholder="Rua Lugar Algum" title="Escreva o nome da sua rua"/>
                     </div>
                     <div class="w-2/12">
-                        <x-form.input-group label="Número" name="ENDERECO_NUMERO" placeholder="Número" type="number" title="Escreva o número do seu endereço"/>
+                        <x-form.input-group id="numero" label="Número" name="ENDERECO_NUMERO" placeholder="Número" type="number" title="Escreva o número do seu endereço"/>
                     </div>
                 </div>
                 <div class="flex justify-between">
                     <div class="w-4/12">
-                        <x-form.input-group label="Bairro" name="ENDERECO_BAIRRO" placeholder="Bairro" type="number" title="Escreva o seu bairro"/>
+                        <x-form.input-group id="bairro" label="Bairro" name="ENDERECO_BAIRRO" placeholder="Bairro" title="Escreva o seu bairro"/>
                     </div>
                     <div class="w-5/12">
-                        <x-form.input-group label="Cidade" name="ENDERECO_CIDADE" placeholder="Cidade" title="Escreva a sua cidade"/>
+                        <x-form.input-group id="cidade" label="Cidade" name="ENDERECO_CIDADE" placeholder="Cidade" title="Escreva a sua cidade"/>
                     </div>
                     <div class="w-2/12">
-                        <x-form.input-group label="Estado" name="ENDERECO_ESTADO" placeholder="Estado" title="Escreva o seu estado"/>
+                        <x-form.input-group id="estado" label="Estado" name="ENDERECO_ESTADO"
+                            onkeydown="return /[A-Z]/i.test(event.key)"
+                            oninput="this.value = this.value.toUpperCase()"
+                            maxlength="2"
+                            placeholder="Estado"
+                            title="Escreva o seu estado"/>
                     </div>
                 </div>
                 <div class="flex justify-between">
                     <div class="w-6/12">
-                        <x-form.input-group label="Logradouro" name="ENDERECO_NOME" placeholder="Casa, Trabalho, Casa da praia..." title="Escreva o nome do endereço"/>
+                        <x-form.input-group id="nome_end" label="Nome do endereço" name="ENDERECO_NOME" placeholder="Casa, Trabalho, Casa da praia..." title="Escreva o nome do endereço"/>
                     </div>
                     <div class="w-5/12">
-                        <x-form.input-group label="Complemento" name="ENDERECO_COMPLEMENTO" placeholder="Bl 01, apto 01" title="Escreva o complemento do endereço"/>
+                        <x-form.input-group id="complemento" label="Complemento" name="ENDERECO_COMPLEMENTO" placeholder="Bl 01, apto 01" title="Escreva o complemento do endereço"/>
                     </div>
                 </div>
                 <div class="flex justify-between items-center my-1 ">
-                    <a onclick="limparCampos()" class="poppins text-azul text-xs hover:underline cursor-pointer">
+                    <p id="limpar" class="poppins text-azul text-xs hover:underline cursor-pointer">
                         Limpar campos
-                    </a>
+                    </p>
                     <x-form.button href="carrinho/pagamento" text="Continuar">
                         Finalizar
                     </x-form.button>
@@ -56,5 +62,5 @@
 
     <x-footer.footer />
 
-    <script src="scripts\layout\carrinho.js"></script>
+    <script src="\scripts\layout\carrinho.js"></script>
 </x-layout>
