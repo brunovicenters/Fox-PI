068b05d213c388d030dde3bc595b3694b10cd6d7
bruno <brunovicente.dev@gmail.com>
2024-04-07 21:01:39 -0300

    Added * to label when required

--- resources/views/components/form/input-group.blade.php
+++ resources/views/components/form/input-group.blade.php
@@ -1,7 +1,7 @@
 @props(['label' => '', 'placeholder' => '', 'name' => '', 'type' => 'text', 'id' => $name, 'required' => true])
 
 <div class="flex flex-col mb-3">
-    <label for="{{ $name }}" class="poppins text-laranja-escuro drop-shadow-md font-semibold">{{ $label }}:</label>
+    <label for="{{ $name }}" class="poppins text-laranja-escuro drop-shadow-md font-semibold">{{ $label }}:{{$required ? '*' : ''}}</label>
     <input
         type="{{ $type }}"
         name="{{ $name }}"
