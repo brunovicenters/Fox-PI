19f21a50216f01c224d2d421b58e84934112d698
Iggor Manfio <iggormanfio4@hotmail.com>
2024-04-15 15:13:45 -0300

    add cards

--- resources/views/tela-inicial/home.blade.php
+++ resources/views/tela-inicial/home.blade.php
@@ -1,4 +1,107 @@
 <x-layout>
     <x-navbar />
     <img src="images/Fox-Banner.png" alt="Banner do site">
+
+    <section class="max-w-8xl flex justify-center items-center">
+    <div class="w-5/6 mt-10 flex flex-col justify-center itens-center gap-8">
+        <h1 class="text-6xl hanalei text-roxo text-left">Mais Vendidos</h1>
+        <div class="flex items-center justify-center gap-7">
+            <div class="flex flex-col h-52 w-40 border-4 border-solid border rounded-3xl color-border">
+                <div class="h-1/2 bg-white rounded-t-3xl"></div>
+                <div class="h-1/2 bg-color-amarelo rounded-b-3xl flex flex-col justify-center items-center">
+                    <h1 class="text-xl hanalei text-laranja-claro">Nome</h1>
+                    <p>Categoria</p>
+                    <p>R$ 00,00</p>
+                    <p>R$ 00,00</p>
+                </div>
+            </div>
+            <div class="flex flex-col h-52 w-40 border-4 border-solid rounded-3xl color-border">
+                <div class="h-1/2 bg-white rounded-t-3xl"></div>
+                <div class="h-1/2 bg-color-amarelo rounded-b-3xl flex flex-col justify-center items-center">
+                    <h1 class="text-xl hanalei text-laranja-claro">Nome</h1>
+                    <p>Categoria</p>
+                    <p>R$ 00,00</p>
+                    <p>R$ 00,00</p>
+                </div>
+            </div>
+            <div class="flex flex-col h-52 w-40 border-4 border-solid rounded-3xl color-border">
+                <div class="h-1/2 bg-white rounded-t-3xl"></div>
+                <div class="h-1/2 bg-color-amarelo rounded-b-3xl flex flex-col justify-center items-center">
+                    <h1 class="text-xl hanalei text-laranja-claro">Nome</h1>
+                    <p>Categoria</p>
+                    <p>R$ 00,00</p>
+                    <p>R$ 00,00</p>
+                </div>
+            </div>
+            <div class="flex flex-col h-52 w-40 border-4 border-solid rounded-3xl color-border">
+                <div class="h-1/2 bg-white rounded-t-3xl"></div>
+                <div class="h-1/2 bg-color-amarelo rounded-b-3xl flex flex-col justify-center items-center">
+                    <h1 class="text-xl hanalei text-laranja-claro">Nome</h1>
+                    <p>Categoria</p>
+                    <p>R$ 00,00</p>
+                    <p>R$ 00,00</p>
+                </div>
+            </div>
+            <div class="flex flex-col h-52 w-40 border-4 border-solid rounded-3xl color-border">
+                <div class="h-1/2 bg-white rounded-t-3xl"></div>
+                <div class="h-1/2 bg-color-amarelo rounded-b-3xl flex flex-col justify-center items-center">
+                    <h1 class="text-xl hanalei text-laranja-claro">Nome</h1>
+                    <p>Categoria</p>
+                    <p>R$ 00,00</p>
+                    <p>R$ 00,00</p>
+                </div>
+            </div>
+        </div>
+        <h1 class="text-6xl hanalei text-roxo text-left">Promoção</h1>
+        <div class="flex items-center justify-center gap-7">
+            <div class="flex flex-col h-52 w-40 border-4 border-solid rounded-3xl color-border">
+                <div class="h-1/2 bg-white rounded-t-3xl"></div>
+                <div class="h-1/2 bg-color-amarelo rounded-b-3xl flex flex-col justify-center items-center">
+                    <h1 class="text-xl hanalei text-laranja-claro">Nome</h1>
+                    <p>Categoria</p>
+                    <p>R$ 00,00</p>
+                    <p>R$ 00,00</p>
+                </div>
+            </div>
+            <div class="flex flex-col h-52 w-40 border-4 border-solid rounded-3xl color-border">
+                <div class="h-1/2 bg-white rounded-t-3xl"></div>
+                <div class="h-1/2 bg-color-amarelo rounded-b-3xl flex flex-col justify-center items-center">
+                    <h1 class="text-xl hanalei text-laranja-claro">Nome</h1>
+                    <p>Categoria</p>
+                    <p>R$ 00,00</p>
+                    <p>R$ 00,00</p>
+                </div>
+            </div>
+            <div class="flex flex-col h-52 w-40 border-4 border-solid rounded-3xl color-border">
+                <div class="h-1/2 bg-white rounded-t-3xl"></div>
+                <div class="h-1/2 bg-color-amarelo rounded-b-3xl flex flex-col justify-center items-center">
+                    <h1 class="text-xl hanalei text-laranja-claro">Nome</h1>
+                    <p>Categoria</p>
+                    <p>R$ 00,00</p>
+                    <p>R$ 00,00</p>
+                </div>
+            </div>
+            <div class="flex flex-col h-52 w-40 border-4 border-solid rounded-3xl color-border">
+                <div class="h-1/2 bg-white rounded-t-3xl"></div>
+                <div class="h-1/2 bg-color-amarelo rounded-b-3xl flex flex-col justify-center items-center">
+                    <h1 class="text-xl hanalei text-laranja-claro">Nome</h1>
+                    <p>Categoria</p>
+                    <p>R$ 00,00</p>
+                    <p>R$ 00,00</p>
+                </div>
+            </div>
+            <div class="flex flex-col h-52 w-40 border-4 border-solid rounded-3xl color-border">
+                <div class="h-1/2 bg-white rounded-t-3xl"></div>
+                <div class="h-1/2 bg-color-amarelo rounded-b-3xl flex flex-col justify-center items-center">
+                    <h1 class="text-xl hanalei text-laranja-claro">Nome</h1>
+                    <p>Categoria</p>
+                    <p>R$ 00,00</p>
+                    <p>R$ 00,00</p>
+                </div>
+            </div>
+        </div>
+        
+
+    </div>
+    </section>
 </x-layout>
