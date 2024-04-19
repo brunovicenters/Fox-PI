73939a5928a118a863890139eefbc78a5c741a21
bruno <brunovicente.dev@gmail.com>
2024-04-18 23:38:41 -0300

    Added text-truncate + title

--- resources/views/components/navbar/categoria.blade.php
+++ resources/views/components/navbar/categoria.blade.php
@@ -1,5 +1,5 @@
 {{-- TODO: config href --}}
-<div>
+<div class="truncate max-w-2xl" title="{{ $categoria->CATEGORIA_NOME }}">
     <a class="hanalei text-3xl text-vermelho">
         {{ $categoria->CATEGORIA_NOME }}
     </a>
