5d535570440cf310dc89531f1d1be352f860b8d5
bruno <brunovicente.dev@gmail.com>
2024-04-26 14:50:16 -0300

    Added fale conosco controler

---
+++ app/Http/Controllers/FaleConoscoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FaleConoscoController extends Controller
+{
+    public function index()
+    {
+        $options = [
+            "Produto danificado",
+            "Não recebi meu produto",
+            "Recebi o produto errado",
+            "Troca de produto",
+            "Não consigo achar o produto que quero",
+            "Outro"
+        ];
+
+        return view('fale-conosco.index', [
+            'options' => $options
+        ]);
+    }
+}
