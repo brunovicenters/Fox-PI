05613c9ec40b33ad9db36eb4d854bafd045b6362
Iggor Manfio <iggormanfio4@hotmail.com>
2024-04-29 15:32:34 -0300

    Arrumei carrinho + seta

--- resources/views/produto/produto.blade.php
+++ resources/views/produto/produto.blade.php
@@ -33,25 +33,26 @@
                         <p class="mt-1 text-2xl">1</p>
                         <button class="text-2xl">+</button>
                     </div>
-                    <div class="flex gap-3 relative">
-                            <span class="absolute -top-3 -left-2 text-xl rounded-full flex justify-center items-center w-8 h-8 p-1 z-50 text-amarelo shopcart-icon-number">+</span>
-                            <button class="flex itens-center justify-center text-white rounded-lg px-10 py-2 w-1/2 font-bold drop-shadow-md  hover:scale-105 hover:drop-shadow-lg btn-car text-x h-11">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 -scale-x-100">
+                    <div class="flex gap-3">
+                        <div class="relative">
+                            <button class="flex items-center justify-center text-white rounded-lg px-10  w-full font-bold drop-shadow-md  hover:scale-105 hover:drop-shadow-lg btn-car text-x h-11">
+                                <span class="absolute top-1 left-8 text-base rounded-full flex justify-center items-center w-4 h-4 p-1 z-50 text-amarelo shopcart-icon-number">+</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 -scale-x-100">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                                 </svg>
                             </button>
-
+                        </div>
                         <button type="submit" class="flex itens-center justify-center text-laranja-escuro rounded-lg px-10 py-2 w-1/2 poppins drop-shadow-md  hover:scale-105 hover:drop-shadow-lg h-11 text-xl btn-buy">Comprar</button>
                     </div>
                     <div class="flex gap-2">
-                    <input name="cep" type="number" placeholder="CEP" title="CEP" class="p-2 rounded-lg drop-shadow-md text-laranja-escuro w-2/3"></input>
-                        <button class="flex itens-center justify-center rounded-lg px-10 py-2 w-1/3 font-bold drop-shadow-md  hover:scale-105 hover:drop-shadow-lg btn-cep h-11 ">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <input name="cep" type="number" placeholder="CEP" title="CEP" class="p-2 rounded-lg drop-shadow-md text-laranja-escuro w-2/3"></input>
+                        <button id="btnFrete" class="flex items-center justify-center rounded-lg px-10  w-1/3 font-bold drop-shadow-md  hover:scale-105 hover:drop-shadow-lg btn-cep h-11 ">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="8" stroke="currentColor" class="w-10 h-10">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                            </svg>
+                            </svg>  
                         </button>
                     </div>
-                    <p class="poppins text-vermelho">Frete mínimo de R$ 15,00</p>
+                    <p class="poppins text-vermelho" id="valorFrete">Frete mínimo de R$ 10,00</p>
                 </div>
         </div>
     </section>
