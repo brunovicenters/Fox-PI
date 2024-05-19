12245119576b0ae555bd1bb595bdd3a8dedaf113
bruno <brunovicente.dev@gmail.com>
2024-05-19 12:38:39 -0300

    Changed absolute to fixed

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -5,7 +5,7 @@
 
 @if ($type == 'error')
 
-    <div id="toast-danger" class="absolute bottom-1 right-2 flex items-center w-full max-w-xs p-4 mb-4 text-red-800 bg-red-300 rounded-lg shadow toasts z-50" role="alert">
+    <div id="toast-danger" class="fixed bottom-1 right-2 flex items-center w-full max-w-xs p-4 mb-4 text-red-800 bg-red-300 rounded-lg shadow toasts z-50" role="alert">
         <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-lg bg-red-800 text-red-200">
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
@@ -23,7 +23,7 @@
 
 @elseif ($type == 'success')
 
-    <div id="toast-success" class="absolute bottom-1 right-3 flex items-center w-full max-w-xs p-4 mb-4 text-green-800 bg-green-300 rounded-lg shadow toasts z-50" role="alert">
+    <div id="toast-success" class="fixed bottom-1 right-3 flex items-center w-full max-w-xs p-4 mb-4 text-green-800 bg-green-300 rounded-lg shadow toasts z-50" role="alert">
         <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-lg bg-green-800 text-green-200">
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
@@ -41,7 +41,7 @@
 
 @elseif ($type == 'alert')
 
-    <div id="toast-warning" class="absolute bottom-1 right-3 flex items-center w-full max-w-xs p-4 mb-4 text-orange-700 bg-orange-300 rounded-lg shadow toasts z-50" role="alert">
+    <div id="toast-warning" class="fixed bottom-1 right-3 flex items-center w-full max-w-xs p-4 mb-4 text-orange-700 bg-orange-300 rounded-lg shadow toasts z-50" role="alert">
         <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-lg bg-orange-700 text-orange-100">
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z"/>
