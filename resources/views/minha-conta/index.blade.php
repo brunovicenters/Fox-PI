b3f6d811231ea295e3f53d828b2e3ac9a669acd5
bruno <brunovicente.dev@gmail.com>
2024-05-19 12:40:03 -0300

    updated link

--- resources/views/minha-conta/index.blade.php
+++ resources/views/minha-conta/index.blade.php
@@ -63,7 +63,7 @@
             <div class="flex w-full justify-between items-center mb-5">
                 <h2 class="hanalei text-5xl drop-shadow-md text-azul">Meus endereços</h2>
 
-                <a title="Adicione um endereço" href="{{ route("endereco.create") }}" class="rounded-lg p-1 drop-shadow-md bg-green-700 hover:bg-green-900 hover:scale-105 hover:drop-shadow-lg ease-linear">
+                <a title="Adicione um endereço" href="{{ route("endereco.create", "minha-conta") }}" class="rounded-lg p-1 drop-shadow-md bg-green-700 hover:bg-green-900 hover:scale-105 hover:drop-shadow-lg ease-linear">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="white" class="w-8 h-8">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
