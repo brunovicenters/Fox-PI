4260c4d149de2e69ef2b3f3222be3f175fcc4e41
bruno <brunovicente.dev@gmail.com>
2024-05-19 11:38:07 -0300

    Added go back button

--- resources/views/endereco/create.blade.php
+++ resources/views/endereco/create.blade.php
@@ -5,6 +5,14 @@
 
 <x-layout>
 
+    <div class="flex">
+        <a href="{{ route('page.minha-conta') }}" class="bg-btn-sign p-1 rounded-lg mb-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="white" class="w-8 h-8">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+            </svg>
+        </a>
+    </div>
+
     <x-card-horizontal>
         <form action="{{ route('endereco.store') }}" method="POST" class="w-full flex flex-col p-1">
             @csrf
