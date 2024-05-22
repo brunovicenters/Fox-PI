2faf12eec8c0543dc9f82d44122146cb31c16766
bruno <brunovicente.dev@gmail.com>
2024-05-22 14:51:51 -0300

    Fixed close button toats

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -31,7 +31,7 @@
             <span class="sr-only">Check icon</span>
         </div>
         <div class="ms-3 text-sm font-normal">{{$message}}</div>
-        <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-green-800 text-green-200 hover:text-green-100 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-green-600 inline-flex items-center justify-center h-8 w-8" data-dismiss-target="#toast-danger" aria-label="Close">
+        <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-green-800 text-green-200 hover:text-green-100 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-green-600 inline-flex items-center justify-center h-8 w-8" data-dismiss-target="#toast-success" aria-label="Close">
             <span class="sr-only">Close</span>
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
@@ -49,7 +49,7 @@
             <span class="sr-only">Warning icon</span>
         </div>
         <div class="ms-3 text-sm font-normal">{{$message}}</div>
-        <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-orange-700 text-orange-100 hover:text-orange-50 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-orange-600 inline-flex items-center justify-center h-8 w-8" data-dismiss-target="#toast-danger" aria-label="Close">
+        <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-orange-700 text-orange-100 hover:text-orange-50 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-orange-600 inline-flex items-center justify-center h-8 w-8" data-dismiss-target="#toast-alert" aria-label="Close">
             <span class="sr-only">Close</span>
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
