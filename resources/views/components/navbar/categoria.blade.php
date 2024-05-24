aae6bd44134aa8ce2155e16b7a3d6af85ffd5912
bruno <brunovicente.dev@gmail.com>
2024-05-24 17:07:10 -0300

    Fixed distribution categoria

--- resources/views/components/navbar/categoria.blade.php
+++ resources/views/components/navbar/categoria.blade.php
@@ -1,5 +1,5 @@
 {{-- TODO: config href --}}
-<div class="truncate max-w-44 px-2" title="{{ $categoria->CATEGORIA_NOME }}">
+<div class="truncate w-1/8 max-w-44 px-2" title="{{ $categoria->CATEGORIA_NOME }}">
     <a class="hanalei text-2xl text-vermelho" title="{{ $categoria->CATEGORIA_NOME }}" href="#">
         {{ $categoria->CATEGORIA_NOME }}
     </a>
