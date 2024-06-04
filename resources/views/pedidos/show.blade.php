a3c7d5034b554dbc8cbcc78d76e99e67455cba96
bruno <brunovicente.dev@gmail.com>
2024-06-04 11:08:45 -0300

    Added url previous to go back button

--- resources/views/pedidos/show.blade.php
+++ resources/views/pedidos/show.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
 
     <div class="flex">
-        <a href="{{ route('pedidos.index') }}" class="bg-btn-sign p-1 rounded-lg mb-4">
+        <a href="{{ url()->previous() }}" class="bg-btn-sign p-1 rounded-lg mb-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="white" class="w-8 h-8">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
             </svg>
--- resources/views/produto/produto.blade.php
+++ resources/views/produto/produto.blade.php
@@ -6,7 +6,7 @@
 
 <x-layout>
     <div class="flex">
-        <a href="{{ route('home') }}" class="bg-btn-sign p-1 rounded-lg mb-4">
+        <a href="{{ url()->previous() }}" class="bg-btn-sign p-1 rounded-lg mb-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="white" class="w-8 h-8">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
             </svg>
