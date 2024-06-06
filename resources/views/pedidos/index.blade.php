a936280170c93be17e10f1e4d6fec73f539c6487
bruno <brunovicente.dev@gmail.com>
2024-06-06 19:46:26 -0300

    Added if in case ther is no pedidos

--- resources/views/pedidos/index.blade.php
+++ resources/views/pedidos/index.blade.php
@@ -1,12 +1,15 @@
 <x-layout>
-
-    <h1 class="hanalei text-6xl drop-shadow-md uppercase mb-5 text-azul">Meus pedidos</h1>
-    <div class="flex flex-col space-y-3">
-        @foreach ($pedidos as $pedido)
-            <a href="{{ route('pedidos.show', $pedido) }}" class="pointer hover:scale-105 ease-in-out duration-300 w-3/4">
-                <x-pedidos.card-pedidos :pedido="$pedido" />
-            </a>
-        @endforeach
-    </div>
+    @if($pedidos->count() > 0)
+        <h1 class="hanalei text-6xl drop-shadow-md uppercase mb-5 text-azul">Meus pedidos</h1>
+        <div class="flex flex-col space-y-3">
+            @foreach ($pedidos as $pedido)
+                <a href="{{ route('pedidos.show', $pedido) }}" class="pointer hover:scale-105 ease-in-out duration-300 w-3/4">
+                    <x-pedidos.card-pedidos :pedido="$pedido" />
+                </a>
+            @endforeach
+        </div>
+    @else
+        <p class="text-vermelho text-center">Não foram encontrados registros</p>
+    @endif
 
 </x-layout>
